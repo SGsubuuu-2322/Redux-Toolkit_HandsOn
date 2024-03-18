1c8c78958323779a12335599c8620cddb431e404
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-18 12:24:37 +0530

    Created a reducer and configured it with store and then wrap it onto App component tag...

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import CounterSlice from "../Reducers/CounterSlice";
 
 configureStore({
-  reducer: {},
+  reducer: {
+    counterReducer: CounterSlice,
+  },
 });
