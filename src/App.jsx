4ccd7f5fafb30ab86e50287be276b5f07e047f3c
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-18 11:34:23 +0530

    Installed redux-toolkit and created central redux store...

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="container bg-red-200 m-auto p-10 mt-5">
+      <h1 className="text-4xl text-red-900 text-center font-bold">
+        Let&apos;s Start Redux Toolkit...
+      </h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
